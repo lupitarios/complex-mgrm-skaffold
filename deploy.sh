73709c064c0defd1f3ae6b7cb9dcb3b4992607a1
Maria Maldonado <mariag.riosm@gmail.com>
2023-12-03 12:17:23 -0500

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t m4r14r1os/complex-multi-client:latest -t m4r14r1os/complex-multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t m4r14r1os/complex-multi-server:latest -t m4r14r1os/complex-multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t m4r14r1os/complex-multi-worker:latest -t m4r14r1os/complex-multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push m4r14r1os/complex-multi-client:latest
+docker push m4r14r1os/complex-multi-server:latest
+docker push m4r14r1os/complex-multi-worker:latest
+
+docker push m4r14r1os/complex-multi-client:$SHA
+docker push m4r14r1os/complex-multi-server:$SHA
+docker push m4r14r1os/complex-multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=m4r14r1os/complex-multi-server:$SHA
+kubectl set image deployments/client-deployment server=m4r14r1os/complex-multi-client:$SHA
+kubectl set image deployments/worker-deployment server=m4r14r1os/complex-multi-worker:$SHA
